e9c3ee49d50e1f6da6ffa0727db5da5d067c42a4
HIMADRI MANNA <91056721+Himadrimanna@users.noreply.github.com>
2023-05-24 14:36:27 +0530

    Update signup_controller.dart

--- lib/screens/Signup/signup_controller.dart
+++ lib/screens/Signup/signup_controller.dart
@@ -1,19 +1,3 @@
-// // import 'package:e_cart/screens/authentication/authentication_repo.dart';
-// // import 'package:flutter/widgets.dart';
-// // import 'package:get/get.dart';
-
-// // class Signupcontroller extends GetxController {
-// //   static Signupcontroller get instance => Get.find();
-
-// //   final fullname = TextEditingController();
-// //   final email = TextEditingController();
-// //   final phoneno = TextEditingController();
-// //   final password = TextEditingController();
-
-// //   void registeruser(String email, String password) {
-// //     AuthenticationRepo.instance.createUserWithEmailAndPassword(email, password);
-// //   }
-// // }
 
 import 'package:e_cart/screens/authentication/authentication.functions.dart';
 import 'package:e_cart/screens/authentication/authentication_repo.dart';
